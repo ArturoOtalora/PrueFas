9e584ecb3501bbb4e33a43841617170e4e9f4707
Skoutperiferia16 <Skoutperiferia16@Skandia.com.co>
2025-09-17 16:48:47 -0500

    se actualiza correo

--- main.py
+++ main.py
@@ -6781,12 +6781,12 @@ async def descargar_pdf_Premium(usuario_id: int):
 
     try:
         await aiosmtplib.send(
-            #  message,
-            #  hostname="smtp.gmail.com",
-            #  port=587,
-            #  start_tls=True,
-            #  username="correopruebavital@gmail.com",
-            #  password="cxvi hyne temx xmgt"
+             message,
+             hostname="smtp.gmail.com",
+             port=587,
+             start_tls=True,
+             username="correopruebavital@gmail.com",
+             password="olxh cdfd lsmo skcz"
         )
     except Exception as e:
         print(f"Error al enviar el correo: {e}")
@@ -6813,12 +6813,12 @@ async def descargar_pdf(usuario_id: int):
 
     try:
         await aiosmtplib.send(
-            #  message,
-            #  hostname="smtp.gmail.com",
-            #  port=587,
-            #  start_tls=True,
-            #  username="correopruebavital@gmail.com",
-            #  password="cxvi hyne temx xmgt"
+             message,
+             hostname="smtp.gmail.com",
+             port=587,
+             start_tls=True,
+             username="correopruebavital@gmail.com",
+             password="olxh cdfd lsmo skcz"
         )
     except Exception as e:
         print(f"Error al enviar el correo: {e}")
@@ -6847,12 +6847,12 @@ async def enviar_pdf_email(usuario_id: int = Form(...), correo_destino: str = Fo
     # Envía el correo
     try:
         await aiosmtplib.send(
-        #    message,
-        #    hostname="smtp.gmail.com",
-        #     port=587,
-        #    start_tls=True,
-        #     username="correopruebavital@gmail.com",
-        #    password="cxvi hyne temx xmgt"
+           message,
+           hostname="smtp.gmail.com",
+            port=587,
+           start_tls=True,
+            username="correopruebavital@gmail.com",
+           password="olxh cdfd lsmo skcz"
         )
         return {"mensaje": f"PDF enviado a {correo_destino} correctamente."}
     except Exception as e:
